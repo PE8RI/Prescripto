b02f4e9fac441ab7d0efec77261dea4177d357e2
PE8RI <ritwickraj5632@gmail.com>
2026-02-01 01:12:19 +0530

    added doctors list

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../componets/Header'
 import SpecialityMenu from '../componets/SpecialityMenu'
+import TopDoctors from '../componets/TopDoctors'
 
 const Home = () => {
   return (
     <div>
        <Header/>
        <SpecialityMenu/>
+       <TopDoctors/>
     </div>
   )
 }
