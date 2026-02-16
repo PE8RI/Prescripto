03555aa2c7c16dbe0ea7fd7ae82862922860dba9
PE8RI <ritwickraj5632@gmail.com>
2026-02-17 03:34:16 +0530

    added contact and Login/signup page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
 import About from './pages/About'
+import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointments from './pages/Appointments'
@@ -19,6 +20,7 @@ const App = () => {
         <Route path='/doctors/:speciality' element={<Doctors/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
         <Route path='/my-profile' element={<MyProfile/>}/>
         <Route path='/my-appointment' element={<MyAppointments/>}/>
         <Route path='/appointment/:docId' element={<Appointments/>}/>
