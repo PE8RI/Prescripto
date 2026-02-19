eb61114721c48d147d0fc7997c7e02f9e8cbfec2
PE8RI <ritwickraj5632@gmail.com>
2026-02-20 02:08:53 +0530

    created doctor and user Schema and also done wuth the setup of Cloudinary,MongoDB Atlas

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
- 
+dotenv.config();
+
 
 const app = express();
+const PORT = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
@@ -20,7 +26,7 @@ app.get("/api/test", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+ 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
