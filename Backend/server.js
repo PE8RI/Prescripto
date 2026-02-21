a9ae36c3b6b908369084d0f1a6b60811107445b0
PE8RI <ritwickraj5632@gmail.com>
2026-02-21 23:22:05 +0530

    Fix doctor creation API: correct form-data fields, improve address parsing, and stabilize image upload handling

--- Backend/server.js
+++ Backend/server.js
@@ -3,17 +3,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 dotenv.config();
 
-
+//app config
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
+//Middlewares
 app.use(cors());
 app.use(express.json());
+
+
+//api endpoints
+app.use("/api/admin",adminRouter);
+
 
 app.get("/", (req, res) => {
   res.send("Server is running successfully 🚀");
