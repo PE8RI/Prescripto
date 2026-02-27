11590fea7cb32f9d899ca0c3c4c1faec42fb8626
PE8RI <ritwickraj5632@gmail.com>
2026-02-27 23:37:45 +0530

    added admin auth and Created admin app

---
+++ Admin/src/App.jsx
@@ -0,0 +1,14 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+    <h1 className="text-white text-3xl font-bold">
+      Admin Panel Ready 🚀
+    </h1>
+  </div>
+   )
+ }
+ 
+ export default App
+ 
