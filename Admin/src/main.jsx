51434d1bc71fb2c44bad6e5192de9c30b7de2347
PE8RI <ritwickraj5632@gmail.com>
2026-03-03 04:40:39 +0530

    implement admin login with context, token persistence and toast notifications

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -4,17 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AdminContextProvider from './context/AdminContext.jsx'
-import { DoctorContext } from './context/DoctorContext.jsx'
-import { AppContext } from './context/AppContext.jsx'
+import  DoctorContextProvider  from './context/DoctorContext.jsx'
+import  AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+<BrowserRouter>
   <AdminContextProvider>
-    <DoctorContext>
-      <AppContext>
+    <DoctorContextProvider>
+      <AppContextProvider>
       <App />
-      </AppContext>
-    </DoctorContext>
+      </AppContextProvider>
+    </DoctorContextProvider>
   </AdminContextProvider>
 </BrowserRouter>,
 )
--- Admin/src/pages/Login.jsx
+++ Admin/src/pages/Login.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
 const Login = () => {
-  const [state, setState] = useState('Admin')
+  const [state, setState] = useState("Admin")
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const {setAToken,backendUrl} =useContext(AdminContext)
+
+  const onSubmitHandler= async (event)=>{
+      event.preventDefault()
+
+      try{
+        if(state === 'Admin'){
+          
+          const{data}=await axios.post(backendUrl + '/api/admin/login',{email,password})
+          if(data.success){
+            localStorage.setItem('aToken',data.token)
+            setAToken(data.token)
+          } else {
+            toast.error(data.message)
+          }
+
+
+        }else{
+
+        }
+      }catch(error){
+
+      }
+  }
 
 return (
-  <form className='min-h-[80vh] flex items-center'>
+  <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
     <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
 
       <p className='text-2xl font-semibold m-auto'>
@@ -13,7 +43,7 @@ return (
 
       <div className='w-full'>
         <p>Email</p>
-        <input
+        <input onChange={(e)=>setEmail(e.target.value)} value={email}
           className='border border-[#DADADA] rounded w-full p-2 mt-1'
           type='email'
           required
@@ -22,7 +52,7 @@ return (
 
       <div className='w-full'>
         <p>Password</p>
-        <input
+        <input onChange={(e)=>setPassword(e.target.value)} value={password}
           className='border border-[#DADADA] rounded w-full p-2 mt-1'
           type='password'
           required
