ba5e8d4dcae16beed835a1be84b9996ac9b836f0
PE8RI <ritwickraj5632@gmail.com>
2026-03-07 05:25:03 +0530

    add user authentication APIs and implement doctor availability toggle with backend API and admin panel integration

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -89,5 +89,16 @@ const loginAdmin = async (req, res) => {
 }
 
 // API to get all doctors list for admin panel
+const allDoctors = async (req, res) => {
+  try {
 
-export { addDoctor,loginAdmin}
+      const doctors = await doctorModel.find({}).select('-password')
+      res.json({ success: true, doctors })
+
+  } catch (error) {
+      console.log(error)
+      res.json({ success: false, message: error.message })
+  }
+}
+
+export { addDoctor,loginAdmin,allDoctors}
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -19,7 +21,9 @@ app.use(express.json());
 
 
 //api endpoints
-app.use("/api/admin",adminRouter);
+app.use("/api/admin",adminRouter)
+app.use("/api/doctor",doctorRouter)
+app.use('/api/user',userRouter)
 
 
 app.get("/", (req, res) => {
